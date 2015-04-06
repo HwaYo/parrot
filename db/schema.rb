22172b9c411275ef9143f55d2c57a359a0d3db4f
DevSusu <os1742@gmail.com>
2015-04-06 19:27:51 +0900

    commit to checkout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406062030) do
+ActiveRecord::Schema.define(version: 20150406073852) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "color"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(version: 20150406062030) do
     t.text     "bookmark"
     t.string   "file"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "deleted",    default: false
   end
 
   add_index "records", ["user_id"], name: "index_records_on_user_id"
