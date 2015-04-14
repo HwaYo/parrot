df5d9b4784adb55579d23a8382c5c527dcf213ad
Hong ChulJu <ang0123dev@gmail.com>
2015-04-14 23:20:50 +0900

    users have default bookmarks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,13 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
+
+    if user.nil?
+      user = User.create_with_omniauth(auth)
+      create_bookmark!(user)
+    end
+
     session[:user_id] = user.id
     redirect_to records_path
   end
@@ -9,5 +15,14 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to root_url, :notice => "Signed out!"
+  end
+
+private
+  def create_bookmark!(user)
+    [
+      { name: 'Important', color: '#e11d21' },
+      { name: "Don't Understand", color: '#fbca04' },
+      { name: 'Not Important', color: '#207de5' }
+    ].each { |attribute| user.bookmarks.create!(attribute) }
   end
 end
