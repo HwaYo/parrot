ed0391c7d848e7d07297ebba2000a702ebbc833d
Hong ChulJu <ang0123dev@gmail.com>
2015-05-13 10:16:16 +0900

    Test api added. (will be squashed)

--- app/api/api.rb
+++ app/api/api.rb
@@ -2,11 +2,23 @@ require 'doorkeeper/grape/helpers'
 
 class API < Grape::API
   helpers Doorkeeper::Grape::Helpers
+  helpers do
+    def current_user
+      User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+    end
+  end
 
   prefix 'api'
   version 'v1', using: :path
+  format :json
 
   before do
     doorkeeper_authorize!
+  end
+
+  get 'records' do
+    current_user.records.map do |record|
+      record.attributes.except("file", "created_at", "updated_at")
+    end
   end
 end
