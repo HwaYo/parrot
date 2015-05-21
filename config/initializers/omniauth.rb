aeb7d7f588ba569508798075120d4e263b091da5
Hong ChulJu <ang0123dev@gmail.com>
2015-05-21 17:28:23 +0900

    Facebook app id, secret from env variable.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '369921866546593', '5d89571bfbdfbd2873a2b7be4bc809b7', image_size: 'square'
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], image_size: 'square'
 end
